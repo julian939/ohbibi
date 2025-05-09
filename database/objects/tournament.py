26259ec8534b2d94e29983ff8c7e3d810210c758
Julian Kalmes <juliankalmes@mac.speedport.ip>
2025-05-09 22:47:45 +0200

    0.0.4

--- database/objects/tournament.py
+++ database/objects/tournament.py
@@ -1,4 +1,8 @@
 import database.sqlmanager as sql
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class Tournament:
     def __init__(self, tournament_id: int):
@@ -31,3 +35,47 @@ class Tournament:
 
     def delete(self):
         self.db.execute("DELETE FROM tournaments WHERE tournament_id = ?", (self.tournament_id,))
+
+    def get_team_size(self):
+        return self.team_size
+
+    def set_team_size(self, new_team_size: int):
+        try:
+            self.db.execute("UPDATE tournaments SET team_size = ? WHERE tournament_id = ?", (new_team_size, self.tournament_id))
+            self.team_size = new_team_size
+            logger.info(f"team_size updated to '{new_team_size}' for tournament_id {self.tournament_id}")
+        except Exception as e:
+            logger.error(f"Error updating team_size: {e}")
+
+    def get_title(self):
+        return self.title
+
+    def set_title(self, new_title: str):
+        try:
+            self.db.execute("UPDATE tournaments SET title = ? WHERE tournament_id = ?", (new_title, self.tournament_id))
+            self.title = new_title
+            logger.info(f"title updated to '{new_title}' for tournament_id {self.tournament_id}")
+        except Exception as e:
+            logger.error(f"Error updating title: {e}")
+
+    def get_date(self):
+        return self.date
+
+    def set_date(self, new_date: int):
+        try:
+            self.db.execute("UPDATE tournaments SET date = ? WHERE tournament_id = ?", (new_date, self.tournament_id))
+            self.date = new_date
+            logger.info(f"date updated to '{new_date}' for tournament_id {self.tournament_id}")
+        except Exception as e:
+            logger.error(f"Error updating date: {e}")
+
+    def get_is_official(self):
+        return self.is_official
+
+    def set_is_official(self, new_is_official: int):
+        try:
+            self.db.execute("UPDATE tournaments SET is_official = ? WHERE tournament_id = ?", (new_is_official, self.tournament_id))
+            self.is_official = new_is_official
+            logger.info(f"is_official updated to '{new_is_official}' for tournament_id {self.tournament_id}")
+        except Exception as e:
+            logger.error(f"Error updating is_official: {e}")
